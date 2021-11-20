ab782681c3774e7dee995bb7f38744007e0b306f
Cesar Ballardini <cesar.ballardini@gmail.com>
2021-11-20 20:20:48 -0300

    fix: cuando faalta alguna dependencia o se corto la instalacion de un paquete previo

--- provision/instala-requisitos.sh
+++ provision/instala-requisitos.sh
@@ -3,10 +3,12 @@ export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
 export APT_OPTIONS=' -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold '
 
-
 export HOST_IP_ADDRESS=${1:-192.168.56.10}
 export WP_DOMAIN=${2:-wpdev.virtual.ballardini.com.ar}
 export HOSTNAME=${3:-wpdev}
+
+sudo dpkg --configure -a
+sudo apt-get install -f ${APT_OPTIONS}
 
 sudo apt-get install pwgen ${APT_OPTIONS}
 sudo apt-get install net-tools ${APT_OPTIONS}
