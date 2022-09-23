995af5dfbbff97b3917aa632656ae3ff5234e2c3
Cesar Ballardini <cesar.ballardini@gmail.com>
2022-09-23 00:35:48 -0300

    se puede seleccionar versión de PHP y Wordpress entre las disponibles en las descargas

--- provision/vars.sh
+++ provision/vars.sh
@@ -9,4 +9,29 @@ export WP_DB_PASSWORD="wordpress"
 export MYSQL_ROOT_PASSWORD="root"
 #export MYSQL_ROOT_PASSWORD="$(pwgen -1 -s 64)"
 
+# https://www.php.net/supported-versions.php 
+# Branch Initial Release Active Support Until Security Support Until
+# 7.4    28 Nov 2019     28 Nov 2021          28 Nov 2022
+# 8.0    26 Nov 2020     26 Nov 2022          26 Nov 2023
+# 8.1    25 Nov 2021     25 Nov 2023          25 Nov 2024
+
 PHP_VERSION=7.4
+PHP_VERSION=8.0
+PHP_VERSION=8.1
+
+
+# https://wordpress.org/download/releases/
+WORDPRESS_VERSION=latest
+WORDPRESS_VERSION=6.0.2
+
+
+##
+# ultimas versiones (2022-09-22)
+#PHP_VERSION=8.1
+#WORDPRESS_VERSION=latest
+
+
+##
+# las versiones instaladas son:
+PHP_VERSION=7.4
+WORDPRESS_VERSION=5.7.7
